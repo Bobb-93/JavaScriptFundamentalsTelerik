6b151a5fc45c1d9ef1a424226252ec8a9cc1d406
Bobb-93 <borisiakimov@gmail.com>
2025-03-25 22:11:09 +0200

    add BudgetTrackerApp -> main.js

---
+++ BudgetTrackerApp/main.js
@@ -0,0 +1,9 @@
+let totalIncome =0;
+let totalExpense = 0;
+let balance = 0;
+
+let transaction1 = "Income: 500, Salary";
+let transaction2 = "Expense: 50, Dog food";
+let transaction3 = "Expense: 70, Cat food";
+let transaction4 = "Income: 100, Bonus";
+
