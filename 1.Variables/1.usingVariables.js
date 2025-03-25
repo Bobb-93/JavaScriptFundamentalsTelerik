a0f5b8725baaa31480b1fd64ea9a2c7e195d8dfc
Bobb-93 <borisiakimov@gmail.com>
2025-03-25 21:45:44 +0200

    add 1.usingVariables.js

---
+++ 1.Variables/1.usingVariables.js
@@ -0,0 +1,32 @@
+// declaring var, let, const
+
+var age = 25;
+var userName = "Ivan";
+var isAdult = true;
+
+let favoriteColor = "blue";
+let birthYear = 1995;
+
+const PI = 3.14;
+
+console.log("Възраст: ", age);
+console.log("Потребителско име: ", userName);
+
+age=30;
+userName = "Gosho";
+
+console.log("Възраст: ", age);
+console.log("Потребителско име: ", userName);
+
+// PI = 4;
+
+let myVar = "Hello";
+console.log(myVar);
+// console.log(MyVar);
+
+let firstName = "John";
+let lastName = "Doe";
+let fullName = firstName + ' ' + lastName;
+
+console.log("Пълно име: ", fullName);
+
