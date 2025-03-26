d2e32de76c48d098b27683f043bce24594c546ae
Bobb-93 <borisiakimov@gmail.com>
2025-03-26 15:15:32 +0200

    add 3.Operators -> 1.Hours.js

---
+++ 3.Operators/1.Hours.js
@@ -0,0 +1,6 @@
+let minutes = 137;
+let hours = parseInt(minutes / 60);
+let remainingMinutes = minutes % 60;
+ 
+console.log(`${minutes} minutes is equal to `);
+console.log(`${hours} hours and ${remainingMinutes} minutes`);
