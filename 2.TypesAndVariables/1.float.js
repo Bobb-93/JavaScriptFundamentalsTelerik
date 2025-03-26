8af8cfd8dc2561ade4929abef2c3a730b71e91ff
Bobb-93 <borisiakimov@gmail.com>
2025-03-26 11:46:21 +0200

    add 2.TypesAndVariables -> 1.float.js

---
+++ 2.TypesAndVariables/1.float.js
@@ -0,0 +1,2 @@
+let floatingPoint = console.log((0.3).toFixed(20));
+// 0.29999999999999998890
---
+++ 2.TypesAndVariables/2.parse.js
@@ -0,0 +1,10 @@
+let not_a_number = Number('25apple'); // NaN
+let nullString = String(null); // 'null'
+let trueFromString = Boolean('Marry'); // true
+let falseFromEmpty = Boolean(''); // false
+let sasa0= Boolean(0); // false
+let sasa1= Boolean(5); // false
+
+console.log(sasa0);
+console.log(sasa1);
+
