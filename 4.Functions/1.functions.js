277b5a9f13193dd72b835a88217914ee9a257f34
Bobb-93 <borisiakimov@gmail.com>
2025-03-26 22:55:49 +0200

    add Functions -> functions.js

---
+++ 4.Functions/1.functions.js
@@ -0,0 +1,9 @@
+let rounded = Math.round(5.5)    // 6
+let minValue = Math.min(2, 5, -33)   // 2
+let maxValue = Math.max(-2, 16);
+
+console.log(rounded);
+console.log(minValue);
+console.log(maxValue);
+
+
