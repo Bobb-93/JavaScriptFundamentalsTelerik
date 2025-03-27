fae700d6cde7c8d5b5e6356dd3bfcf22faa4ac5d
Bobb-93 <borisiakimov@gmail.com>
2025-03-27 15:07:14 +0200

    add 10.DataTasksPack5 -> 3.QuadraticEquation.js

---
+++ 10.DataTasksPack5/3.QuadraticEquation.js
@@ -0,0 +1,24 @@
+let input = [
+    '2',
+    '5',
+    '-3',
+];
+
+let print = this.print || console.log;
+let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
+
+let a = +gets();
+let b = +gets();
+let c = +gets();
+
+// let D = (b * b) - (4*a*c);
+let D = b * b - 4*a*c;
+
+let x1 = (-b - Math.sqrt(D)) / (2*a);
+let x2 = (-b + Math.sqrt(D)) / (2*a);
+
+// let x1 = -b - Math.sqrt(D) / (2*a);
+// let x2 = -b + Math.sqrt(D) / (2*a);
+
+print(`x1=${x1.toFixed(1)}`);
+print(`x2=${x2.toFixed(1)}`);
