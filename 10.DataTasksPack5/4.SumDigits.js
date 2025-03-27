4c4c032c8595bbe153a907a2b0ed7af0d8d0441e
Bobb-93 <borisiakimov@gmail.com>
2025-03-27 15:14:51 +0200

    add 10.DataTasksPack5 -> 4.SumDigits.js

---
+++ 10.DataTasksPack5/4.SumDigits.js
@@ -0,0 +1,16 @@
+let input = [
+    '2346',
+];
+
+let print = this.print || console.log;
+let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
+
+let N = +gets();
+
+let firstDigit = Math.floor(N / 1000); // 2
+let secondDigit = Math.floor(N / 100 % 10); // 23 % 10 = 3
+let thirdDigit = Math.floor(N / 10 % 10); // 234 % 10
+let fourthDigit = N % 10;
+
+let sumDigits = firstDigit + secondDigit + thirdDigit + fourthDigit;
+print(sumDigits);
