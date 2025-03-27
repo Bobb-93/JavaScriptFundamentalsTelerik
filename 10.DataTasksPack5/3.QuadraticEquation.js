93b9eb5a853d3cb234940d04ac858820989935a2
Bobb-93 <borisiakimov@gmail.com>
2025-03-27 16:36:14 +0200

    add 10.DataTasksPack5 -> 3.QuadraticEquation.js - parseFloat

--- 10.DataTasksPack5/3.QuadraticEquation.js
+++ 10.DataTasksPack5/3.QuadraticEquation.js
@@ -7,9 +7,14 @@ let input = [
 let print = this.print || console.log;
 let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
 
-let a = +gets();
-let b = +gets();
-let c = +gets();
+// let a = +gets();
+// let b = +gets();
+// let c = +gets();
+
+let a = parseFloat(gets());
+let b = parseFloat(gets());
+let c = parseFloat(gets());
+
 
 // let D = (b * b) - (4*a*c);
 let D = b * b - 4*a*c;
