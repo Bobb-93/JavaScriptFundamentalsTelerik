d76e2f6c0ca33a51d5fd14edba9fe786b261ea68
Bobb-93 <borisiakimov@gmail.com>
2025-03-27 16:38:23 +0200

    add 10.DataTasksPack5 -> 4.SumDigits.js - telerik way

--- 10.DataTasksPack5/4.SumDigits.js
+++ 10.DataTasksPack5/4.SumDigits.js
@@ -5,12 +5,18 @@ let input = [
 let print = this.print || console.log;
 let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
 
-let N = +gets();
+let N = gets();
 
-let firstDigit = Math.floor(N / 1000); // 2
-let secondDigit = Math.floor(N / 100 % 10); // 23 % 10 = 3
-let thirdDigit = Math.floor(N / 10 % 10); // 234 % 10
-let fourthDigit = N % 10;
+//without array - my solution
+// let N = +gets();
 
-let sumDigits = firstDigit + secondDigit + thirdDigit + fourthDigit;
+// let firstDigit = Math.floor(N / 1000); // 2
+// let secondDigit = Math.floor(N / 100 % 10); // 23 % 10 = 3
+// let thirdDigit = Math.floor(N / 10 % 10); // 234 % 10
+// let fourthDigit = N % 10;
+
+// let sumDigits = firstDigit + secondDigit + thirdDigit + fourthDigit;
+
+//Telerik way
+let sumDigits = +N[0] + +N[1] + +N[2] + +N[3];
 print(sumDigits);
