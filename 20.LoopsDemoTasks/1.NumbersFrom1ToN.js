8d6987b024f49815b9712abef49cbfca3bcc7517
Bobb-93 <borisiakimov@gmail.com>
2025-03-29 21:43:32 +0200

    add 20.LoopsDemoTasks -> 1.NumbersFrom1ToN.js

---
+++ 20.LoopsDemoTasks/1.NumbersFrom1ToN.js
@@ -0,0 +1,20 @@
+let input = [
+    '5',
+];
+
+let print = this.print || console.log;
+let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
+
+let N = +gets();
+
+let result = '';
+
+for (let i = 1; i <= N; i++){
+    if(i === N){
+        result += i;
+        break;
+    }
+    result += i + ' ';
+}
+
+console.log(result);
