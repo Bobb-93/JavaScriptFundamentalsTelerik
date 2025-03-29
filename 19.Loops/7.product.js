ab44df9c5ad1c44c46d0604422b7d6c196795b68
Bobb-93 <borisiakimov@gmail.com>
2025-03-29 21:30:32 +0200

    edit 19.Loops -> 7.product.js

---
+++ 19.Loops/7.product.js
@@ -0,0 +1,17 @@
+let n = 3;
+let m = 6;
+
+if (n < m) {
+  let num = n;
+  let product = 1;
+  while (num <= m) {
+    product = product * num;
+    console.log(`product = ${product}`);
+    
+    num++;
+  }
+
+  console.log(`Product[${n}..${m}] = ${product}`);
+} else {
+  console.log(`Error: n (${n}) should be smaller than m (${m})!`);
+}
