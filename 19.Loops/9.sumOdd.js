1234239335e22f88dc77ba2180e22da151e47d72
Bobb-93 <borisiakimov@gmail.com>
2025-03-29 21:32:12 +0200

    edit 19.Loops -> 9.sumOdd.js

---
+++ 19.Loops/9.sumOdd.js
@@ -0,0 +1,14 @@
+let n = 11;
+let sum = 0;
+
+for (let i = 1; i <= n; i++) {
+  if (i % 2 === 0) {
+    continue;
+  }
+
+  console.log(i);
+  
+  sum = sum + i;
+}
+
+console.log(`Sum of odd numbers from 1 to ${n} = ${sum}`);
