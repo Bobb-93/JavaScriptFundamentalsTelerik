9ca675773ba8b06072c05ee7faff1d3be26de774
Bobb-93 <borisiakimov@gmail.com>
2025-04-07 12:42:15 +0300

    add 27.LoopsTasksPack2 -> 2.CalculateDiscount.js

---
+++ 27.LoopsTasksPack2/2.CalculateDiscount.js
@@ -0,0 +1,24 @@
+let input = [
+    // '2',
+    // '50',
+    // '40',
+    '4',
+    '9.99',
+    '19.99',
+    '29.99',
+    '39.99',
+];
+
+let print = this.print || console.log;
+let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
+
+let N = +gets();
+
+for (let i = 0; i < N; i++) {
+
+    let price = parseFloat(gets());
+
+    // console.log(Math.round(price * 0.35));
+    console.log((price * 0.35).toFixed(2));
+    
+}
