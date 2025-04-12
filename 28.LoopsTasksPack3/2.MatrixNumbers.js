3c9a1b5b304fce4ccd6da540e51f83b60d8a6eee
Bobb-93 <borisiakimov@gmail.com>
2025-04-12 13:12:41 +0300

    edit 28.LoopsTasksPack3 -> 2.MatrixNumbers.js

--- 28.LoopsTasksPack3/2.MatrixNumbers.js
+++ 28.LoopsTasksPack3/2.MatrixNumbers.js
@@ -2,19 +2,20 @@ let input = [
     // '2',
     // '50',
     // '40',
-    '3',
-    '2',
+    '5',
 ];
 
 let print = this.print || console.log;
 let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
 
 let N = +gets();
-let counter = 1; 
 
-for (let i = 1; i <= N-1; i++) {
-    for (let j = 1; j <= N-1; j++) {
-        print(`${j} `);
+for (let i = 0; i < N; i++) {
+    let row = '';
+    for (let j = 0; j < N; j++) {
+        row += (i + j + 1) + ' ';
+        console.log(`row: ${row}`);
+        
     }
-    print();
+    console.log(row.trim());
 }
---
+++ 28.LoopsTasksPack3/3.PrimeFactors.js
@@ -0,0 +1,9 @@
+let input = [
+    '12',
+];
+
+let print = this.print || console.log;
+let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
+
+let N = +gets();
+
