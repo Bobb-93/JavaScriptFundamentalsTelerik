62dbd52c246ff2e3c68384bdbca7d372b6fd66db
Bobb-93 <borisiakimov@gmail.com>
2025-04-12 13:22:50 +0300

    add 28.LoopsTasksPack3 -> 3.PrimeFactors.js

--- 28.LoopsTasksPack3/3.PrimeFactors.js
+++ 28.LoopsTasksPack3/3.PrimeFactors.js
@@ -6,4 +6,15 @@ let print = this.print || console.log;
 let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
 
 let N = +gets();
+let divisor = 2;
+// let factors = [];
+
+while (N > 1) {
+    if(N % divisor === 0){
+        console.log(divisor);
+        N /= divisor;
+    } else{
+        divisor++;
+    }
+}
 
