366209fc2d29bd368e8f3cf984b7048a01f4fdd5
Bobb-93 <borisiakimov@gmail.com>
2025-06-13 15:20:10 +0300

    add 30.LoopsTasksPack6 -> 1.NumberWave.js

---
+++ 31.LoopsTasksPack6/1.NumberWave.js
@@ -0,0 +1,22 @@
+let input = [
+    '28',
+];
+
+let print = this.print || console.log;
+let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
+
+let N = +gets();
+
+let outputArray = [];
+
+for (let i = 1; i <= N; i++) {
+    outputArray += `${i} `;
+}
+
+for (let i = N - 1; i >= 1; i--) {
+    outputArray += `${i} `;
+}
+
+outputArray = outputArray.slice(0, -1);
+
+print(outputArray);
