b6059481cbc1488eacd94e9c0f5a930a8452dec1
Bobb-93 <borisiakimov@gmail.com>
2025-06-19 16:38:34 +0300

    edit 33.ArraysTasksPack2 -> 2.Negative-PositiveSort.js

---
+++ 33.ArraysTasksPack2/2.Negative-PositiveSort.js
@@ -0,0 +1,27 @@
+let input = [
+    '7 2 -2 1 -5 4 5 -3 1',
+];
+
+let print = this.print || console.log;
+let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
+
+let array = gets().split(' ').map(Number);
+
+let arrayLength = array.length;
+
+let negativeArray = [];
+let positiveArray = [];
+
+for (let i = 0; i < arrayLength; i++) {
+    const element = array[i];
+    
+    if(element < 0){
+        negativeArray.push(element);
+    } else{
+        positiveArray.push(element);
+    }
+}
+
+let resultArray = negativeArray.concat(positiveArray);
+
+print(resultArray.join(" "));
