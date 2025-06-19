bc5eca10ba040d851daa2085bf6fee8295ad899b
Bobb-93 <borisiakimov@gmail.com>
2025-06-19 19:32:10 +0300

    add 34.ArraysTasksPack3 -> 4.StrangeOrder.js

---
+++ 34.ArraysTasksPack3/4.StrangeOrder.js
@@ -0,0 +1,30 @@
+let input = [
+    '3,-12,0,0,13,5,1,0,-2',
+];
+
+let print = this.print || console.log;
+let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
+
+let array = gets().split(',').map(Number);
+
+let negativeArray = [];
+let positiveArray = [];
+let zeroArray = [];
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+
+    if (element === 0) {
+        zeroArray.push(element);
+    } else if (element < 0) {
+        negativeArray.push(element);
+    } else {
+        positiveArray.push(element);
+    }
+
+}
+
+let resultArray = negativeArray.concat(zeroArray, positiveArray);
+
+print(resultArray.join());
+
